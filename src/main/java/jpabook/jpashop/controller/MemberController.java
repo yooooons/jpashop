cf88bcf67f6bb55d33f83e83288c141e71ab13a4
unknown <yooooonnf@gmail.com>
2022-03-30 21:51:28 +0900

    웹 계층 개발

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm",new MemberForm());
+        return "members/createMemberFrom";
+
+    }
+    @PostMapping("/members/new")
+    public String createForm(@Validated MemberForm form, BindingResult result) {
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        if (result.hasErrors()) {
+            return "members/createMemberFrom";
+        }
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
