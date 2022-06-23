a885d64f4e09052d0beb56f0b56e446524baec50
unknown <yooooonnf@gmail.com>
2022-06-23 22:53:08 +0900

    api jpashop

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +22,7 @@ public class MemberApiController {
 
 
     @PostMapping("/api/v2/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Validated CreateMemberRequest request) {
+    public CreateMemberResponse saveMemberV2(@RequestBody @Validated CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
 
@@ -31,7 +30,34 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable Long id,
+            @RequestBody @Validated UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(),findMember.getName());
+
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+
     //DTO) CreateMemberRequest
+
     @Data
     static class CreateMemberRequest {
         private String name;
