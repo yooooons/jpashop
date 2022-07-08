71accfc387313fddc9e5ece9edfebf17f205fb62
unknown <yooooonnf@gmail.com>
2022-07-08 23:09:33 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -15,6 +15,9 @@ public class JpashopApplication {
 
 	@Bean
 	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		//강제 지연 로딩 설정
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
 		return new Hibernate5Module();
 	}
 
